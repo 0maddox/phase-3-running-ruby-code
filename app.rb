a7c3b8ae8f732bfa7e41b3de5eabf8237072a8bf
nicholas <nickkiim7@gmail.com>
2022-10-18 00:28:38 +0300

    completed assignment

---
+++ app.rb
@@ -0,0 +1,8 @@
+#app.rb
+puts "Hello World!"
+print "Pass this test, please."
+
+
+p [ 1, 2, 3]
+
+pp [{id: 1, hello: "world"},{id: 2, hello: "moon"},{id: 3, hello: "learner"},{id: 4, hello: "ruby"}]
